ab2a318ab5a79483f7d1b6b46d63a4a12af06a63
Suraj Chauhan <83372389+chauhanhere@users.noreply.github.com>
2022-07-24 02:04:47 +0530

    Create 315.Count of smaller number after self.py

---
+++ 315.Count of smaller number after self.py
@@ -0,0 +1,48 @@
+def countSmaller(self, nums: List[int]) -> List[int]:
+        ans=[0 for i in range(len(nums))]
+        
+        def mergeSort(nums):
+            if len(nums)>1:
+                mid=len(nums)//2
+                L=nums[:mid]
+                R=nums[mid:]
+                mergeSort(L)
+                mergeSort(R)
+                
+                # Merging Starts
+                i,j,k=0,0,0
+                while(i<len(L) and j<len(R)):
+                    if L[i][0]>R[j][0]:
+                        ans[L[i][1]]+=len(R)-j
+                        nums[k]=L[i]
+                        i+=1
+                    else:
+                        nums[k]=R[j]
+                        j+=1
+                    k+=1
+                while(i<len(L)):
+                    nums[k]=L[i]
+                    k+=1
+                    i+=1
+                while(j<len(R)):
+                    nums[k]=R[j]
+                    j+=1
+                    k+=1
+        
+        for i in range(len(nums)):
+            nums[i]=[nums[i],i]
+        mergeSort(nums)
+        return ans
+        
+        
+        
+        #TLE it is made by myself and passed 14 test cases.
+        ''' 
+       c=[]
+        for i in range(len(nums)):
+            count=0
+            for j in range(i,len(nums)):
+                if nums[j]<nums[i]:
+                    count+=1
+            c.append(count)
+        return c'''
